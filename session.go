36fa9f247b9006156f98f95ba2520cda3fb94545
kcmvp <kcheng.mvp@gmail.com>
2020-09-30 02:02:30 +0800

    add mapper and settings

--- session.go
+++ session.go
@@ -1,10 +1,37 @@
 package sqlx
 
+import (
+	"context"
+	"database/sql"
+)
 
-
+// Session is an interface that defines methods for database adapters.
 type Session interface {
+	// Name returns the name of the database.
+	Name() string
 
-	Exec(mapper Mapper)
+	// Ping returns an error if the DBMS could not be reached.
+	Ping() error
 
+
+	// Close terminates the currently active connection to the DBMS and clears
+	// all caches.
+	Close() error
+
+	Driver() interface{}
+
+	Tx(fn func(sess Session) error) error
+
+	TxContext(ctx context.Context, fn func(sess Session) error, opts *sql.TxOptions) error
+
+	Context() context.Context
+
+	WithContext(ctx context.Context) Session
+
+	ExecInsert(mapper Mapper, arg interface{}) (sql.Result, error)
+	ExecUpdate(mapper Mapper, arg interface{}) (sql.Result, error)
+	Select(dest interface{}, mapper Mapper, args ...interface{}) error
+	Get(dest interface{}, mapper Mapper, args ...interface{}) error
+
+	Settings
 }
-
