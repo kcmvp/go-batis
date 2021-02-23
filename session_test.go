37603ee145437c860b911f3a1073d2452962ced5
kcmvp <kcheng.mvp@gmail.com>
2021-02-23 21:12:38 +0800

    add session test

--- session_test.go
+++ session_test.go
@@ -2,17 +2,38 @@ package batis
 
 import (
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"testing"
 )
 
-func TestNewSession(t *testing.T) {
+type SessionTestSuit struct {
+	suite.Suite
+	DbSession *Session
+}
+
+func (s SessionTestSuit) BeforeTest(suiteName, testName string) {
+	//clear up the data
+}
+
+func (s SessionTestSuit) SetupSuite() {
+	s.Assert().NotNil(s.DbSession)
+	// init schema
+}
+
+func TestExampleTestSuite(t *testing.T) {
 	cfg := DbConfig{
 		Url:        "file::memory:?cache=shared",
 		DriverName: "sqlite3",
 	}
-	assertion := assert.New(t)
-	session, err := NewSession(&cfg)
-	assertion.NotNil(session)
-	assertion.Nil(err)
+	if session, err := NewSession(&cfg); err == nil {
+		suite.Run(t, &SessionTestSuit{DbSession: session})
+	}
 }
+
+func (s *SessionTestSuit) TestName() {
+	s.Equal(1,2)
+}
+
+
+
+
